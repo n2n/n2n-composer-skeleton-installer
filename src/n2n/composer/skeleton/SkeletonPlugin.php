bf40d16e19995b00b5d52fa2fe64ee1748d393d1
Andreas von Burg <a@von-burg.net>
2016-08-22 15:19:02 +0200

    project init

---
+++ src/n2n/composer/skeleton/SkeletonPlugin.php
@@ -0,0 +1,13 @@
+<?php
+namespace n2n\composer\skeleton;
+
+use Composer\Plugin\PluginInterface;
+use Composer\IO\IOInterface;
+
+class SkeletonPlugin implements PluginInterface {
+	
+	public function activate(\Composer\Composer $composer, IOInterface $io) {
+		$installer = new ModuleInstaller($io, $composer);
+		$composer->getInstallationManager()->addInstaller($installer);
+	}
+}
